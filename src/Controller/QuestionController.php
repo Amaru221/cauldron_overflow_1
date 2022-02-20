286c1d6f92cfc9045e9adb87a70199ca344ff826
Amaru221 <agd1923@gmail.com>
2022-02-20 20:35:21 +0100

    Trying use and configurate the prod and dev env

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
+use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Cache\CacheInterface;
 use Twig\Environment;
 
 class QuestionController extends AbstractController
@@ -24,9 +26,9 @@ class QuestionController extends AbstractController
     /**
      * @Route("/questions/{slug}", name="app_question_show")
      */
-    public function show($slug){
+    public function show($slug, MarkdownParserInterface $markdownParser, CacheInterface $cache){
 
-        dump($slug, $this);
+        //dump($slug, $this);
 
         $answers = [
             'Make sure your cat is sitting purrrfectly still?',
@@ -34,10 +36,21 @@ class QuestionController extends AbstractController
             'Maybe.. try saying the spell backwards?',
         ];
 
+        $questionText = 'I\'ve been turned into a cat, any *thoughts* on how to turn back? While I\'m **adorable**, I don\'t really care for cat food.';
+        //$parsedQuestionText = $markdownParser->transformMarkdown($questionText);
+
+        //using cache service
+        $parsedQuestionText = $cache->get('markdown_'.md5($questionText), function() use ($questionText, $markdownParser){
+            return $markdownParser->transformMarkdown($questionText);
+        });
+
+        //dd($markdownParser);
+        dump($cache);
 
         return $this->render('question/show.html.twig', [
             'question' => ucwords(str_replace('-',' ', $slug)),
             'answers' => $answers,
+            'questionText' => $parsedQuestionText,
         ]);
     }
 }
