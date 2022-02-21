617c7e78370c2e48cbac878e1669776f1aa0fa09
Amaru221 <agd1923@gmail.com>
2022-02-21 17:01:01 +0100

    named autowiring, binding global arguments, fetching non-autowireable services and controllers with services

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
@@ -10,6 +11,22 @@ use App\Service\MarkdownHelper;
 
 class QuestionController extends AbstractController
 {
+
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+    /**
+     * @var bool
+     */
+    private $isDebug;
+
+    public function __construct(LoggerInterface $logger, bool $isDebug){
+
+        $this->logger = $logger;
+        $this->isDebug = $isDebug;
+    }
+
     /**
      * @Route("/", name="app_homepage")
      */
@@ -28,7 +45,9 @@ class QuestionController extends AbstractController
     public function show($slug, MarkdownHelper $markdownHelper){
 
         //dump($slug, $this);
-
+        if($this->isDebug){
+            $this->logger->info('we are in debug mode!');
+        }
         $answers = [
             'Make sure your cat is sitting purrrfectly still?',
             'Honestly, I like furry shoes better than MY cat',
@@ -43,6 +62,7 @@ class QuestionController extends AbstractController
 
         //dd($markdownParser);
         //dump($cache);
+        //dump($this->getParameter('cache_adapter'));
 
         return $this->render('question/show.html.twig', [
             'question' => ucwords(str_replace('-',' ', $slug)),
--- src/Service/MarkdownHelper.php
+++ src/Service/MarkdownHelper.php
@@ -2,6 +2,7 @@
 namespace App\Service;
 
 use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Contracts\Cache\CacheInterface;
 
 class MarkdownHelper
@@ -9,14 +10,34 @@ class MarkdownHelper
 
     private $markdownParser;
     private $cache;
-    public function __construct(MarkdownParserInterface $markdownParser, CacheInterface $cache)
+    private $isDebug;
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    public function __construct(MarkdownParserInterface $markdownParser, CacheInterface $cache, bool $isDebug, LoggerInterface $mdLogger)
     {
         $this->markdownParser = $markdownParser;
         $this->cache = $cache;
+        $this->isDebug = $isDebug;
+        $this->logger = $mdLogger;
+        //dump($isDebug);
+
     }
 
     public function parse(string $source):string
     {
+        //logger service, dependency injection
+        if(stripos($source, 'cat') !== false){
+            $this->logger->info('Meow!');
+        }
+
+        //inyeccion de dependencia con valores escalares
+        if ($this->isDebug) {
+            return $this->markdownParser->transformMarkdown($source);
+        }
+
         //using cache service
         return $this->cache->get('markdown_'.md5($source), function() use ($source){
             return $this->markdownParser->transformMarkdown($source);
